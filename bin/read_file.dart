0ebcd978db06744a0ec1e36b385df43093cf4b80
ZARIFMAC <uzbzarif@gmail.com>
2022-10-18 16:54:12 +0500

    init

---
+++ bin/read_file.dart
@@ -0,0 +1,3 @@
+import 'package:path/path.dart' as p;
+
+void main() {}
---
+++ test/read_file_test.dart
@@ -0,0 +1,8 @@
+import 'package:read_file/read_file.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
